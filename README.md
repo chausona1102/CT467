e94f39eb1ae51acbe7f9b55faf3e6c2351c1d051
chausona1102 <nab2205890@student.ctu.edu.vn>
2025-07-04 14:31:52 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+  
+
++++++++++++++++++++++++
+?? CT467 - DATA MANAGEMENT PROJECT
+
+AUTH:
+Student 1: CHAU SO NA
+ID: B2205890
+Student 2: NGUYEN VAN KHA
+ID: B2205877
+
+TIME START: 04/07/2025
+
+
+
+
+=====================================
+|   Repo for study and research     |
+=====================================
---
+++ src/controllers/Controller.php
@@ -0,0 +1,17 @@
+<?php 
+    namespace App\controllers;
+    use League\Plates\Engine;
+
+    class Controller {
+        protected $view;
+
+        public function __construct() {
+            $this->view = new Engine(__DIR__ . '/../views', 'php');
+            $this->view->addFolder('partials', __DIR__ . '/../views/partials');
+        }
+        public function render($template, $data = []) {
+            echo $this->view->render($template, $data);
+        }
+    }
+
+
