9ae4be4c8fbbe3ffa7a2368976bf2d34e13df1ad
chausona1102 <nab2205890@student.ctu.edu.vn>
2025-07-20 14:06:37 +0700

    remake ui for login-signup

--- src/views/signup.php
+++ src/views/signup.php
@@ -11,20 +11,27 @@
     <title>Sign Up</title>
 </head>
 <body>
+    <div id="header">
+        <div id="banner"></div>
+        <h1 class="">Quản lí kí túc xá</h1>
+        <div id="logo">
+            <img src="/images/logo.png" alt="Logo"> <br>
+        </div>
+    </div>
     <div class="container">
         <div class="row d-flex justify-content-center align-items-center w-20">
             <form method="POST" action="/signup">
-                <h1>Đăng ký</h1>
-                <div class="">
+                <legend>Đăng ký</legend>
+                <div class="form-group">
                     <label for="username" class="form-label">Tên đăng nhập</label>
                     <input type="text" class="form-control" id="username" aria-describedby="usernameHelp" name="username"
                         required>
                 </div>
-                <div class="">
+                <div class="form-group">
                     <label for="password" class="form-label">Mật khẩu</label>
                     <input type="password" class="form-control" id="password" name="password" required>
                 </div>
-                <div class="">
+                <div class="form-group">
                     <label for="confirm_password" class="form-label">Xác nhận mật khẩu</label>
                     <input type="password" class="form-control" id="confirm_password" name="confirm_password" required>
                 </div>
